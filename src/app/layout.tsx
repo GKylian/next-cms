27b43c1dd5342a13d1ed3de43725ffdbe090ccb8
Mynnhos <kylian@gauteron.me>
2023-10-06 20:28:19 +0200

    Initial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Next-cms',
+  description: 'Default Next CMS website. Edit metadata in the page.tsx files or the block editor.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
