05d7ac0bf16cbbb29038bed3c9ea9c131be043de
Mynnhos <kylian@gauteron.me>
2023-10-07 21:39:09 +0200

    Added zoom controls in header

--- src/app/admin/editor/page/components/BlocksEditor.tsx
+++ src/app/admin/editor/page/components/BlocksEditor.tsx
@@ -2,14 +2,15 @@
 
 import { useState } from 'react';
 import styles from '../pageEditor.module.scss'
+import { usePageEditorContext } from '../PageEditorContext';
 
 type Props = {}
 function BlocksEditor({ }: Props) {
-    const [zoom, setZoom] = useState(1.0);
+    const { editor } = usePageEditorContext();
 
     return (
         <div className={styles.pagePreview} style={{ backgroundColor: 'white' }}>
-            <div className={styles.blockArea} style={{ zoom: `${zoom}`, width: `1920px` }}>
+            <div className={styles.blockArea} style={{ zoom: `${editor.zoom}`, width: `1920px` }}>
                 <div>1 - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro explicabo aut rerum sed, repudiandae facilis pariatur soluta qui facere iste unde natus voluptatibus fugit officia fugiat placeat, rem minima accusamus?</div>
 
             </div>
--- src/app/admin/editor/page/components/PageEditorHeader.tsx
+++ src/app/admin/editor/page/components/PageEditorHeader.tsx
@@ -7,11 +7,20 @@ import LogoHierarchy from '/public/layer-group-solid.svg'
 import LogoStyling from '/public/fill-drip-solid.svg'
 import LogoCaret from '/public/angle-down-solid.svg'
 import { usePageEditorContext } from '../PageEditorContext'
+import { useEffect, useState } from 'react'
 
 type Props = {}
 function PageEditorHeader({ }: Props) {
     
     const { editor, setEditor, page, setPage } = usePageEditorContext();
+
+    const [zoom, setZoom] = useState<string>(Math.round(editor.zoom * 100) + "%");
+    
+    const updateEditorZoom = (newZoom: number) => {
+        if (!isNaN(newZoom))
+            setEditor((_editor) => ({..._editor, zoom: newZoom}));
+        setZoom(Math.round(newZoom * 100) + "%");
+    }
 
     return (
         <header className={styles.header}>
@@ -26,11 +35,21 @@ function PageEditorHeader({ }: Props) {
                     <LogoHierarchy />
                 </button>
             </div>
+
             <div className={styles.flexGroup}>
-                <input type="text" name="pageNameInput" id="pageNameInput" placeholder="Page Name..."
+                <input className={styles.titleInput} type="text" name="pageNameInput" id="pageNameInput" placeholder="Page Name..."
                     value={page.title} onChange={(e) => setPage((_page) => ({..._page, title: e.target.value || _page.title}))} />
             </div>
+
             <div className={styles.flexGroup}>
+                <div className={styles.zoomControls}>
+                    <button onClick={(e) => updateEditorZoom(editor.zoom-0.1)}>&minus;</button>
+                    <input type="text" inputMode='numeric' name="zoomInput" id="zoomInput" placeholder="1.0"
+                        value={zoom} onChange={(e) => setZoom(e.target.value)}
+                        onBlur={(e) => updateEditorZoom(parseInt(e.target.value) / 100.0)}
+                        onKeyDown={(e) => {if(e.key==="Enter") updateEditorZoom(parseInt(e.currentTarget.value) / 100.0)}}/>
+                    <button onClick={(e) => updateEditorZoom(editor.zoom+0.1)}>+</button>
+                </div>
                 <button className={styles.icon}>
                     <LogoStyling />
                 </button>
