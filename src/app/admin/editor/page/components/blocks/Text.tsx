24095b241f303e2c0cbcf2d99d3a512032e1b527
GKylian <kylian@gauteron.me>
2023-10-12 23:01:35 +0200

    Made text in text block editable

--- src/app/admin/editor/page/components/blocks/Text.tsx
+++ src/app/admin/editor/page/components/blocks/Text.tsx
@@ -4,17 +4,43 @@ import { SortableContext } from "@dnd-kit/sortable"
 import styles from '../../pageEditor.module.scss'
 import { getChildren, getChildrenIds } from "../blocks"
 import SortableBlock from "../SortableBlock"
+import { useEffect, useRef, useState } from "react"
 
 type Props = {
     blocks: BlockData[],
     block: BlockData,
 }
 function Flexbox({ blocks, block }: Props) {
+    const type = {type: block.properties.type || "p"};
 
+    const [isEditing, setIsEditing] = useState(false);
+    const [text, setText] = useState(block.properties.text || "This is a text block. Click here to edit it.");
+
+    const inputRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        if(!isEditing || !inputRef.current) return;
+        adjustInputHeight();
+    }, [isEditing])
+
+    const adjustInputHeight = () => {
+        if(!inputRef.current) return;
+        inputRef.current.style.height = "auto";
+        inputRef.current.style.height = inputRef.current.scrollHeight + "px";
+    }
+    
     return (
-        <p className={`${styles.text}`}>
-            {block.properties.text || "This is a text block. Click here to edit it."}
-        </p>
+        <type.type className={`${styles.text}`}
+            onDoubleClick={() => setIsEditing(true)}>
+            {isEditing ? (
+                <textarea className={`${styles.text}`} value={text} ref={inputRef}
+                    onChange={(e) => { setText(e.target.value); adjustInputHeight(); } }
+                    onBlur={() => setIsEditing(false)}
+                    onKeyDown={(e) => { if(e.key==="Escape") e.currentTarget.blur(); } }/>
+            ) : (
+                <>{text}</>
+            )}
+        </type.type>
     )
 }
 export default Flexbox
